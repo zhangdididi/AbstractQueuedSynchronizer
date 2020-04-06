fb681c6942c80f0110d4ade6fa70a3de36a0f554
zhangdididi <49708573+zhangdididi@users.noreply.github.com>
2020-04-06 12:13:54 +0800

    Update AQS.java

--- AQS.java
+++ AQS.java
@@ -1,33 +1,44 @@
-
 import sun.misc.Unsafe;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.locks.LockSupport;
 
-public class AQS {
-
-    //记录当前加锁的次数
+/**
+ * @author zhangdididi
+ */
+public class Aqs {
+    
+    /**
+     * 记录当前加锁的次数
+     */
     private volatile int state = 0;
-    
-    //当前持有锁的线程
+    /**
+     * 当前持有锁的线程
+     */
     private Thread lockHolder;
-    
-    //线程安全的队列---基于CAS算法
+    /**
+     * 线程安全的队列---基于CAS算法
+     */
     private static ConcurrentLinkedQueue<Thread> waiters = new ConcurrentLinkedQueue<Thread>();
 
     public int getState() {
         return state;
     }
+
     public void setLockHolder(Thread lockHolder) {
         this.lockHolder = lockHolder;
     }
 
-    //尝试获取锁
+    /**
+     * 尝试获取锁
+     */
     public boolean aquire() {
         Thread current = Thread.currentThread();
-        int c = getState();//当前加锁的状态
+        //当前加锁的状态
+        int c = getState();
         if (c == 0) {
             //当前同步器还没有被持有
-            if ((waiters.size() == 0 || current == waiters.peek()) && compareAndSetState(0, 1)) {
+            boolean canUse = waiters.isEmpty() || current == waiters.peek();
+            if (canUse && compareAndSetState(0, 1)) {
                 //修改成功的线程，设置为持有者
                 setLockHolder(current);
                 return true;
@@ -36,7 +47,9 @@ public class AQS {
         return false;
     }
 
-    //加锁
+    /**
+     * 释放锁
+     */
     public void lock() {
         //加锁成功
         if (aquire()) {
@@ -59,7 +72,9 @@ public class AQS {
         }
     }
 
-    //释放锁
+    /**
+     * 释放锁
+     */
     public void unloclk() {
         if (lockHolder != Thread.currentThread()) {
             //抛出异常
@@ -76,20 +91,25 @@ public class AQS {
         }
     }
 
-    //设置加锁状态符号
+
+
     public final boolean compareAndSetState(int expect, int updata) {
         return UNSAFE.compareAndSwapInt(this, STATEOFFSET, expect, updata);
     }
 
-    //通过反射获取一个Unsafe实例对象
+    /**
+     *  通过反射获取一个Unsafe实例对象
+     */
     private static final Unsafe UNSAFE = UnsafeIntance.reflectGetUnsafe();
 
-    //偏移量
+    /**
+     * 偏移量
+     */
     private static final long STATEOFFSET;
 
     static {
         try {
-            STATEOFFSET = UNSAFE.objectFieldOffset(AQS.class.getDeclaredField("state"));
+            STATEOFFSET = UNSAFE.objectFieldOffset(Aqs.class.getDeclaredField("state"));
         } catch (Exception e) {
             throw new Error();
         }
