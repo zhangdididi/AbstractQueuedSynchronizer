48dbd192b369453bbdb2a1ed17a99cae7f3f8f4b
zhangdididi <49708573+zhangdididi@users.noreply.github.com>
2020-04-09 15:24:03 +0800

    Update Aqs.java

--- Aqs.java
+++ Aqs.java
@@ -6,7 +6,7 @@ import java.util.concurrent.locks.LockSupport;
  * @author zhangdididi
  */
 public class Aqs {
-    
+
     /**
      * 记录当前加锁的次数
      */
@@ -15,10 +15,6 @@ public class Aqs {
      * 当前持有锁的线程
      */
     private Thread lockHolder;
-    /**
-     * 线程安全的队列---基于CAS算法
-     */
-    private static ConcurrentLinkedQueue<Thread> waiters = new ConcurrentLinkedQueue<Thread>();
 
     public int getState() {
         return state;
@@ -27,6 +23,11 @@ public class Aqs {
     public void setLockHolder(Thread lockHolder) {
         this.lockHolder = lockHolder;
     }
+
+    /**
+     * 线程安全的队列---基于CAS算法
+     */
+    private static ConcurrentLinkedQueue<Thread> waiters = new ConcurrentLinkedQueue<Thread>();
 
     /**
      * 尝试获取锁
@@ -38,6 +39,7 @@ public class Aqs {
         if (c == 0) {
             //当前同步器还没有被持有
             boolean canUse = waiters.isEmpty() || current == waiters.peek();
+
             if (canUse && compareAndSetState(0, 1)) {
                 //修改成功的线程，设置为持有者
                 setLockHolder(current);
@@ -48,7 +50,7 @@ public class Aqs {
     }
 
     /**
-     * 释放锁
+     * 加锁后的处理
      */
     public void lock() {
         //加锁成功
@@ -56,29 +58,26 @@ public class Aqs {
             return;
         }
         //没有加锁成功
-
-        //cas比较与交换（原子算法）
         Thread current = Thread.currentThread();
         waiters.add(current);
+        //自旋：死循环确保没有获取到锁的线程不再 执行后续代码/占有CPU
         for (;;){
-            //确保没有获取到锁的线程不再执行下面代码/占有CPU
-
             if (aquire()) {
-                waiters.poll();//唤醒队头线程的话，就需要从队列中移除该线程
+                //唤醒队头线程的话，就需要从队列中移除该线程，让后面的线程排到队首
+                waiters.poll();
                 return;
             }
-
-            LockSupport.park();//阻塞--释放CPU使用权  -->RSS
+            LockSupport.park();//阻塞--释放CPU使用权，被刷入到运行时状态段
         }
     }
 
     /**
      * 释放锁
      */
-    public void unloclk() {
+    public void unlock() {
         if (lockHolder != Thread.currentThread()) {
             //抛出异常
-            throw new RuntimeException("lockholder is not current thread");
+            throw new RuntimeException("Lockholder is not current thread");
         }
         int state = getState();
         if (compareAndSetState(state, 0)) {
@@ -91,8 +90,6 @@ public class Aqs {
         }
     }
 
-
-
     public final boolean compareAndSetState(int expect, int updata) {
         return UNSAFE.compareAndSwapInt(this, STATEOFFSET, expect, updata);
     }
@@ -100,7 +97,7 @@ public class Aqs {
     /**
      *  通过反射获取一个Unsafe实例对象
      */
-    private static final Unsafe UNSAFE = UnsafeIntance.reflectGetUnsafe();
+    private static final Unsafe UNSAFE = aqs.UnsafeIntance.reflectGetUnsafe();
 
     /**
      * 偏移量
@@ -109,6 +106,7 @@ public class Aqs {
 
     static {
         try {
+            //获取偏移量
             STATEOFFSET = UNSAFE.objectFieldOffset(Aqs.class.getDeclaredField("state"));
         } catch (Exception e) {
             throw new Error();
